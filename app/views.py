7451d1884561228ba548fae70371e637ef70d760
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-30 22:12:46 +0300

    imported get_articles() function from request module to get business articles from our API

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from  .request import get_articles
 
 #Views
 @app.route('/')
@@ -8,8 +9,12 @@ def index():
   '''
   View root page function that returns the index page and its data
   '''
+
+  #Getting popular news articles
+  business_articles = get_articles('business')
+  print(business_articles)
   title = 'Home - Welcome to The Most Amazing News Highlight Website'
-  return render_template('index.html',title = title)
+  return render_template('index.html',title = title,business_articles = business_articles)
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
