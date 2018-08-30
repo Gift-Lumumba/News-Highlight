0717e30721f9049e3ddf528dc4aacf764f1d29bc
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-30 20:58:11 +0300

    stored base urls

--- app/config.py
+++ app/config.py
@@ -2,7 +2,10 @@ class Config:
     '''
     General configuration parent class
     '''
-    pass
+    ALL_ARTICLES_API_BASE_URL ='https://newsapi.org/v2/everything?sources={}&language=en&sortBy=publishedAt&apiKey={}'
+    HEADLINES_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&language=en&apiKey={}'
+    TECH_NEWS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources=techcrunch&language=en&apiKey={}'
+    BUSINESS_API_BASE_URL ='https://newsapi.org/v2/top-headlines?sources={}&category=business&language=en&apiKey={}'
 
 
 
