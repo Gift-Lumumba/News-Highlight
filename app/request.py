c635b7e2e2722352cb559094655778935cc43845
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-31 16:06:28 +0300

    created get_articles() and process_results() functions that take in article sources as an argument and takes in a list of dictionaries respectively

--- app/request.py
+++ app/request.py
@@ -7,36 +7,29 @@ Article = article.Article
 # Getting api key
 api_key = app.config['ARTICLE_API_KEY']
 
-#Getting the articles base url
-base_url = app.config["ARTICLE_API_BASE_URL"]
+#Getting the article source base url
+# base_url = app.config['SOURCES_API_BASE_URL']
 
 #Getting all articles base url
-articles_url = app.config['ALL_ARTICLES_API_BASE_URL']
+# articles_url = app.config['EVERYTHING_API_BASE_URL']
 
-#Getting headline articles base url
-headlines_url = app.config['HEADLINES_API_BASE_URL']
+#Getting top-headline articles base url
+base_url = app.config['TOP_HEADLINES_API_BASE_URL']
 
-#Getting tech articles base url
-tech_url = app.config['TECH_NEWS_API_BASE_URL']
-
-#Getting business articles base url
-business_url = app.config['BUSINESS_API_BASE_URL']
-
-
-def get_articles(category):
+def get_articles(sources):
     '''
     Function that gets the json response to our url request
     '''
-    get_articles_url = base_url.format(category,api_key)
-
-    with urllib.request.urlopen(get_articles_url) as url:
-        get_articles_data = url.read()
-        get_articles_response = json.loads(get_articles_data)
+    get_sources_url = base_url.format(sources,api_key)
+    print(get_sources_url)
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
 
         article_results = None
 
-        if get_articles_response['results']:
-            article_results_list = get_articles_response['results']
+        if get_sources_response['articles']:
+            article_results_list = get_sources_response['articles']
             article_results = process_results(article_results_list)
 
 
@@ -50,9 +43,9 @@ def process_results(article_list):
         article_list: A list of dictionaries that contain article details
 
     Returns :
-        article_articles: A list of article objects
+        article_results: A list of article objects
     '''
-    article_articles = []
+    article_results = []
     for article_item in article_list:
         author = article_item.get('author')
         title = article_item.get('title')
@@ -63,6 +56,7 @@ def process_results(article_list):
 
         if url:
             article_object = Article(author,title,description,url,urlToImage,publishedAt)
-            article_articles.append(article_object)
+            article_results.append(article_object)
 
-    return article_articles
+    return article_results
+
