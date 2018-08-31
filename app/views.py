95ba2d08f8879eabeb33fe9525346a756b2129b8
Gift-Lumumba <giftlumumba2@gmail.com>
2018-08-31 20:56:13 +0300

    created article object by calling get_article() function and passed in the dynamic URL id

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from  .request import get_articles
+from  .request import get_articles,get_article
 
 #Views
 @app.route('/')
@@ -16,11 +16,15 @@ def index():
   title = 'Home - Welcome to The Most Amazing News Highlight Website'
   return render_template('index.html',title=title,cnn=sources_articles)
 
-@app.route('/article/<int:article_id>')
-def article(article_id):
+@app.route('/article/<int:id>')
+def article(id):
 
     '''
     View article page function that returns the article details page and its data
 
     '''
-    return render_template('article.html',id= article_id)
+
+    article = get_article(id)
+    title = f'{article.title}'
+    
+    return render_template('article.html',title = title ,article = article)
