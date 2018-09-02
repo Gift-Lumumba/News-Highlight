8e53edae462351dbdc01faa5fbb8635dae06a479
Gift-Lumumba <giftlumumba2@gmail.com>
2018-09-02 17:56:29 +0300

    created get_source() function that takes source id and returns a source object

--- app/request.py
+++ app/request.py
@@ -1,9 +1,9 @@
 from app import app
 import urllib.request,json
-from app.models import article,source
+from app.models import source,article
 
-Article = article.Article
 Source = source.Source
+Article = article.Article
 
 # Getting api key
 api_key = app.config['ARTICLE_API_KEY']
@@ -15,7 +15,7 @@ base_url = app.config['SOURCE_API_BASE_URL']
 sports_url = app.config['SPORTS_API_BASE_URL']
 
 # #Getting top-headline articles base url
-# base_url = app.config['TOP_HEADLINES_API_BASE_URL']
+headlines_url = app.config['TOP_HEADLINES_API_BASE_URL']
 
 #Getting business base url
 business_url = app.config['BUSINESS_API_BASE_URL']
@@ -32,20 +32,88 @@ health_url = app.config['HEALTH_API_BASE_URL']
 #Getting science base url
 science_url = app.config['SCIENCE_API_BASE_URL']
 
-def get_articles(sources):
+
+def get_sources(category):
     '''
     Function that gets the json response to our url request
+    Takes news article category as an argument
     '''
-    get_sources_url = base_url.format(sources,api_key)
-    print(get_sources_url)
+    get_sources_url = base_url.format(category,api_key)
+
     with urllib.request.urlopen(get_sources_url) as url:
         get_sources_data = url.read()
         get_sources_response = json.loads(get_sources_data)
 
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
+
+
+    return source_results
+
+def process_sources(source_list):
+    '''
+    Function  that processes the source result and transform them to a list of Objects
+
+    Args:
+        source_list: A list of dictionaries that contain source details
+
+    Returns :
+        source_results: A list of source objects
+    '''
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get('id')
+        name = source_item.get('name')
+        description = source_item.get('description')
+        url = source_item.get('url')
+        category = source_item.get('category')
+        language = source_item.get('language')
+
+        if url:
+            source_object = Source(id,name,description,url,category,language)
+            source_results.append(source_object)
+
+    return source_results
+
+def get_source(id):
+    get_source_details_url = base_url.format(id,api_key)
+
+    with urllib.request.urlopen(get_source_details_url) as url:
+        source_details_data = url.read()
+        source_details_response = json.loads(source_details_data)
+
+        source_object = None
+        if source_details_response:
+            id = source_details_response.get('id')
+            name = source_details_response.get('name')
+            description = source_details_response.get('description')
+            url = source_details_response.get('url')
+            category = source_details_response.get('category')
+            language = source_details_response.get('language')
+
+            source_object = Source(id,name,description,url,category,language)
+
+    return source_object
+
+
+
+def get_articles(sources):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_headlines_url = headlines_url.format(sources,api_key)
+    print(get_headlines_url)
+    with urllib.request.urlopen(get_headlines_url) as url:
+        get_headlines_data = url.read()
+        get_headlines_response = json.loads(get_headlines_data)
+
         article_results = None
 
-        if get_sources_response['articles']:
-            article_results_list = get_sources_response['articles']
+        if get_headlines_response['articles']:
+            article_results_list = get_headlines_response['articles']
             article_results = process_results(article_results_list)
 
 
@@ -213,51 +281,4 @@ def get_science(sources):
 
 
     return science_results
-
-def get_sources(category):
-    '''
-    Function that gets the json response to our url request
-    Takes news article category as an argument
-    '''
-    get_sources_url = base_url.format(category,api_key)
-
-    with urllib.request.urlopen(get_sources_url) as url:
-        get_sources_data = url.read()
-        get_sources_response = json.loads(get_sources_data)
-
-        source_results = None
-
-        if get_sources_response['sources']:
-            source_results_list = get_sources_response['sources']
-            source_results = process_results(source_results_list)
-
-
-    return source_results
-
-def process_sources(source_list):
-    '''
-    Function  that processes the source result and transform them to a list of Objects
-
-    Args:
-        source_list: A list of dictionaries that contain source details
-
-    Returns :
-        source_results: A list of source objects
-    '''
-    source_results = []
-    for source_item in source_list:
-        id = source_item.get('id')
-        name = source_item.get('name')
-        description = source_item.get('description')
-        url = source_item.get('url')
-        category = source_item.get('category')
-        language = source_item.get('language')
-
-        if url:
-            source_object = Source(id,name,description,url,category,language)
-            source_results.append(source_object)
-
-    return source_results
-
-
 
--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from  .request import get_articles,get_article,get_sports,get_business,get_technology,get_entertainment,get_health,get_science
+from  app.request import get_articles,get_article,get_sports,get_business,get_technology,get_entertainment,get_health,get_science,get_sources
 
 #Views
 @app.route('/')
@@ -11,16 +11,22 @@ def index():
   '''
 
   #Getting articles sources
-  sources_articles = get_articles('cnn')
+  article_sources = get_sources('general')
+  print(article_sources)
+  title = 'Home - Welcome to The Most Amazing News Highlight Website'
+  return render_template('index.html', title = title,general = article_sources)
+
+  #Getting articles
+  topheadline_articles = get_articles('cnn')
   sports_articles = get_sports('us')
   business_articles = get_business('gb')
   technology_articles = get_technology('techcrunch')
   entertainment_articles = get_entertainment('entertainment_weekly')
   health_articles = get_health('google_news')
   science_articles = get_science('new_scientist')
-  print(sources_articles)
+  print(topheadline_articles)
   title = 'Home - Welcome to The Most Amazing News Highlight Website'
-  return render_template('index.html',title=title,cnn=sources_articles,us=sports_articles,gb=business_articles,techcrunch=technology_articles,entertainment_weekly=entertainment_articles,google_news=health_articles,new_scientist=science_articles)
+  return render_template('index.html',title=title,cnn=topheadline_articles,us=sports_articles,gb=business_articles,techcrunch=technology_articles,entertainment_weekly=entertainment_articles,google_news=health_articles,new_scientist=science_articles)
 
 @app.route('/article/<int:id>')
 def article(id):
